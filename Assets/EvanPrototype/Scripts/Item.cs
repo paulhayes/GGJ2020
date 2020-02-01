e9a7ab83d508f655a1f6ba821e902c4cd0eded84
Blooker <evangriffiths@hotmail.co.uk>
2020-02-01 11:20:05 +0000

    Started EvanPrototype, has basic movement and item collision detection

---
+++ Assets/EvanPrototype/Scripts/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] LayerMask CanCollect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CanCollect != (CanCollect | 1 << collision.gameObject.layer))
+            return;
+
+        Debug.Log("PICKUP :)");
+    }
+}
---
+++ Assets/EvanPrototype/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed;
+    [SerializeField] Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    public static void Pickup ()
+    {
+
+    }
+
+    void Move (float x, float y)
+    {
+        var movement = new Vector2(x, y).normalized * _moveSpeed;
+        _rb.velocity = movement;
+    }
+}
