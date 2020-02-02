3d93208d835879d78fe0dc13dd196d52aacd4474
Paul Hayes <paul.hayes@entropedia.co.uk>
2020-02-02 07:54:03 +0000

    Added fade in overlay on scavenge state start

---
+++ Assets/Scripts/CameraFade.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraFade : MonoBehaviour
+{
+    [SerializeField] Image background;
+    [SerializeField] GameState _gameState;
+
+    [SerializeField] AnimationCurve fadeCurve;
+
+    [SerializeField] float holdDuration;
+    [SerializeField] float fadeInDuration;
+
+    void Start()
+    {
+        _gameState.StateChangedEvent += OnGameStateChanged;
+    }
+
+    private void OnGameStateChanged(States oldState, States newState)
+    {
+        if(newState==States.Scavenge){
+            CutToBlack();
+            StartCoroutine( FadeIn() );
+        }
+    }
+
+    public IEnumerator FadeIn()
+    {        
+        yield return new WaitForSeconds(holdDuration);
+        var col = background.color;
+        float alpha = col.a;
+        float startAlpha = alpha;
+        float duration = fadeInDuration;
+        while(duration>0){
+            alpha = fadeCurve.Evaluate(duration/fadeInDuration);
+            col.a = alpha;
+            background.color = col;
+            yield return null;
+            duration -= Time.deltaTime;
+        }
+    }
+
+    public void CutToBlack()
+    {
+        var col = background.color;
+        col.a = 1;
+        background.color = col;
+    }
+}
