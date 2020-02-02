6e1fec89edf0690e5136a84139a5f1255453371d
Paul Hayes <paul.hayes@entropedia.co.uk>
2020-02-02 12:16:01 +0000

    Set max height to display on begining screen

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -32,6 +32,12 @@ public class ScoreUI : MonoBehaviour
     void OnGameStateChanged(States oldState, States newState)
     {
         timerSlider.gameObject.SetActive( _gameState.State == States.Scavenge );
+        altitudeReachedField.enabled = _gameState.State == States.Launch || _gameState.State == States.Falling || _gameState.State == States.Begining;
+        if(newState==States.Crash && (playerData.maxAltitudeReached<playerData.altitudeReached))
+        {
+            playerData.maxAltitudeReached = playerData.altitudeReached;
+        }
+        
     }
 
     void Start()
@@ -48,9 +54,9 @@ public class ScoreUI : MonoBehaviour
         
         timerSlider.value = playerData.timeRemaining;
 
-        if(lastHeightDisplayed != playerData.altitudeReached){
-            lastHeightDisplayed = playerData.altitudeReached;
-
+        var altToDisplay = (_gameState.State == States.Begining) ? playerData.maxAltitudeReached : playerData.altitudeReached;
+        if(lastHeightDisplayed != altToDisplay){
+            lastHeightDisplayed = altToDisplay;
             altitudeReachedField.text = Mathf.Round(lastHeightDisplayed*1000).ToString();
         }
     }
