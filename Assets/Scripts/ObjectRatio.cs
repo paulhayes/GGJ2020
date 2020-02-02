6e984996640f3ca9485d3ad3b0aa4c3933035646
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2020-02-02 02:42:35 +0000

    Remove dead files. Consolidated dead zone logic. Collected objects alongside their ratios.

---
+++ Assets/Scripts/ObjectRatio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[Serializable]
+public class ObjectRatio
+{
+    [SerializeField] private GameObject _object;
+    public GameObject Object => _object;
+    [SerializeField] private float _ratio;
+    public float Ratio => _ratio;
+}
