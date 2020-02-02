07d2a51884e9478b12e93aa9401817c1d41d64cf
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2020-02-02 00:57:14 +0000

    Add dead zones. Simplify randomisation.

--- Assets/Scripts/LevelSetup.cs
+++ Assets/Scripts/LevelSetup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Moon;
 using Parts;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class LevelSetup : MonoBehaviour
@@ -18,8 +19,9 @@ public class LevelSetup : MonoBehaviour
     [SerializeField] private BodyPart _bodyPartPrefab;
 
     [SerializeField] private Rect _levelBoundary;
+    [SerializeField] private Rect[] _deadZones;
     [SerializeField] private int _maxCraters; //TODO: These maxes are the actual values at the moment, should we instead randomise within a range?
-    [SerializeField] private int _maxMoonRocks;
+    [FormerlySerializedAs("_maxMoonRocks")] [SerializeField] private int _maxRocks;
     [SerializeField] private int _maxEngineParts;
     [SerializeField] private int _maxFuelParts;
     [SerializeField] private int _maxBodyParts;
@@ -47,100 +49,79 @@ public class LevelSetup : MonoBehaviour
 
     private void SetupMoonFeatures()
     {
-        //Calculate our sizes.
-        var gridWidth = (int)(_levelBoundary.width / _craterPrefab.Size.x);
-        var gridHeight = (int) (_levelBoundary.height / _craterPrefab.Size.y);
-        var totalSize = gridWidth * gridHeight;
-        Debug.Assert(totalSize > _maxCraters, $"Total grid size ({totalSize}) must be greater than the number of craters ({_maxCraters})"); //Warn about an index out of bounds.
-        
-        //Randomly distribute craters around the level.
-        var flatCraterGrid = Enumerable.Repeat(false, totalSize).ToList();
-        for (var i = 0; i < _maxCraters; ++i)
+        //Calculate our cell sizes.
+        var numberOfCellsX = (int)(_levelBoundary.width / (_craterPrefab.Size.x * 2)) - 1; //-1 accounts for the 'last column' which starts at the edge of the screen.
+        var numberOfCellsY = (int) (_levelBoundary.height / (_craterPrefab.Size.y * 2)) - 1;
+        var gridCellWidth = _levelBoundary.width / numberOfCellsX;
+        var gridCellHeight = _levelBoundary.height / numberOfCellsY;
+
+        //Randomly distribute craters around the level, removing any dead zones.
+        var validCells = new List<Rect>();
+        for (var x = 0; x < numberOfCellsX; x++)
         {
-            flatCraterGrid[i] = true;
+            for (var y = 0; y < numberOfCellsY; y++)
+            {
+                var cell = new Rect(_levelBoundary.x + x * gridCellWidth, _levelBoundary.y - y * gridCellHeight, gridCellWidth, gridCellHeight);
+                var touchesDeadZone = false;
+                foreach (var deadZone in _deadZones)
+                {
+                    if (deadZone.Overlaps(cell, true))
+                    {
+                        touchesDeadZone = true;
+                    }
+                }
+
+                if (!touchesDeadZone)
+                {
+                    validCells.Add(cell);
+                }
+            }
         }
-        Shuffle(flatCraterGrid);
+        Shuffle(validCells);
+        var gridPositions = new Queue<Rect>(validCells);
         
         //Create craters in final positions.
-        for(var i = 0; i < totalSize; ++i)
-        {
-            if (!flatCraterGrid[i]) //No crater here, skip on. 
-            {
-                continue;
-            }
-
-            var xJitter = Random.Range(-_craterPositionJitter.x, _craterPositionJitter.x);
-            var yJitter = Random.Range(-_craterPositionJitter.y, _craterPositionJitter.y);
-            var relativeX = (float)i % gridWidth / gridWidth;
-            var relativeY = (float)i / gridWidth / gridHeight + yJitter;
-            var x = _levelBoundary.x + _levelBoundary.width * relativeX + xJitter + _craterPrefab.Size.x / 2;
-            var y = _levelBoundary.y - _levelBoundary.height * relativeY + _craterPrefab.Size.y / 2;
-            var crater = Instantiate(_craterPrefab, new Vector3(x, y, 0), Quaternion.identity);
-        }
-    }
-
-    private enum LevelObject
-    {
-        Rock,
-        EnginePart,
-        FuelPart,
-        BodyPart,
-        None
+        CreatePrefabs(_maxCraters, gridPositions, _craterPositionJitter, _craterPrefab, _craterPrefab.Size);
     }
     
     private void SetupLevel()
     {
         //Calculate our sizes.
-        var gridWidth = (int)(_levelBoundary.width / Mathf.Max(_rockPrefab.Size.x, _enginePartPrefab.Size.x, _fuelPartPrefab.Size.x, _bodyPartPrefab.Size.x));
-        var gridHeight = (int)(_levelBoundary.height / Mathf.Max(_rockPrefab.Size.y, _enginePartPrefab.Size.y, _fuelPartPrefab.Size.y, _bodyPartPrefab.Size.y));
-        var totalSize = gridWidth * gridHeight;
-        var totalObjects = _maxMoonRocks + _maxEngineParts + _maxFuelParts + _maxBodyParts;
-        Debug.Assert(totalSize > totalObjects, $"Total grid size ({totalSize}) must be greater than the total number of objects ({totalObjects})."); //Warn about the index out of bounds.
+        var numberOfCellsX = (int)(_levelBoundary.width / (Mathf.Max(_rockPrefab.Size.x, _enginePartPrefab.Size.x, _fuelPartPrefab.Size.x, _bodyPartPrefab.Size.x) * 2)) - 1;
+        var numberOfCellsY = (int)(_levelBoundary.height / (Mathf.Max(_rockPrefab.Size.y, _enginePartPrefab.Size.y, _fuelPartPrefab.Size.y, _bodyPartPrefab.Size.y) * 2)) - 1;
+        var gridCellWidth = _levelBoundary.width / numberOfCellsX;
+        var gridCellHeight = _levelBoundary.height / numberOfCellsY;
         
         //Randomly distribute objects around the level.
-        var flatGrid = Concatenate(
-            Enumerable.Repeat(LevelObject.Rock, _maxMoonRocks),
-            Enumerable.Repeat(LevelObject.EnginePart, _maxEngineParts),
-            Enumerable.Repeat(LevelObject.FuelPart, _maxFuelParts),
-            Enumerable.Repeat(LevelObject.BodyPart, _maxBodyParts),
-            Enumerable.Repeat(LevelObject.None, totalSize - totalObjects))
-            .ToList();
-        Shuffle(flatGrid);
-        
-        //Create objects in final positions.
-        for(var i = 0; i < totalSize; ++i)
+        var validCells = new List<Rect>();
+        for (var x = 0; x < numberOfCellsX; x++)
         {
-            var xJitter = Random.Range(-_objectPositionJitter.x, _objectPositionJitter.x);
-            var yJitter = Random.Range(-_objectPositionJitter.y, _objectPositionJitter.y);
-            var relativeX = (float)i % gridWidth / gridWidth;
-            var relativeY = (float)i / gridWidth / gridHeight;
-            var x = _levelBoundary.x + _levelBoundary.width * relativeX + xJitter;
-            var y = _levelBoundary.y - _levelBoundary.height * relativeY + yJitter;
-            switch (flatGrid[i]) //No crater here, skip on. 
+            for (var y = 0; y < numberOfCellsY; y++)
             {
-                case LevelObject.Rock:
-                    var rock = Instantiate(_rockPrefab, new Vector3(x + _rockPrefab.Size.x / 2, y + _rockPrefab.Size.y / 2, 0), Quaternion.identity);
-                    break;
-                case LevelObject.EnginePart:
-                    var enginePart = Instantiate(_enginePartPrefab, new Vector3(x + _enginePartPrefab.Size.x / 2, y + _enginePartPrefab.Size.y / 2, 0), Quaternion.identity);
-                    break;
-                case LevelObject.FuelPart:
-                    var fuelPart = Instantiate(_fuelPartPrefab, new Vector3(x + _fuelPartPrefab.Size.x / 2, y + _fuelPartPrefab.Size.y / 2, 0), Quaternion.identity);
-                    break;
-                case LevelObject.BodyPart:
-                    var bodyPart = Instantiate(_bodyPartPrefab, new Vector3(x + _bodyPartPrefab.Size.x / 2, y + _bodyPartPrefab.Size.y / 2, 0), Quaternion.identity);
-                    break;
-                case LevelObject.None:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                var cell = new Rect(_levelBoundary.x + x * gridCellWidth, _levelBoundary.y - y * gridCellHeight, gridCellWidth, gridCellHeight);
+                var touchesDeadZone = false;
+                foreach (var deadZone in _deadZones)
+                {
+                    if (deadZone.Overlaps(cell, true))
+                    {
+                        touchesDeadZone = true;
+                    }
+                }
+
+                if (!touchesDeadZone)
+                {
+                    validCells.Add(cell);
+                }
             }
         }
-    }
-    
-    public static IEnumerable<T> Concatenate<T>(params IEnumerable<T>[] objects)
-    {
-        return objects.SelectMany(i => i);
+        Shuffle(validCells);
+        var gridPositions = new Queue<Rect>(validCells);
+
+        //Create objects in final positions.
+        CreatePrefabs(_maxRocks, gridPositions, _objectPositionJitter, _rockPrefab, _rockPrefab.Size);
+        CreatePrefabs(_maxBodyParts, gridPositions, _objectPositionJitter, _bodyPartPrefab, _bodyPartPrefab.Size);
+        CreatePrefabs(_maxEngineParts, gridPositions, _objectPositionJitter, _enginePartPrefab, _enginePartPrefab.Size);
+        CreatePrefabs(_maxFuelParts, gridPositions, _objectPositionJitter, _fuelPartPrefab, _fuelPartPrefab.Size);
     }
     
     //https://forum.unity.com/threads/clever-way-to-shuffle-a-list-t-in-one-line-of-c-code.241052/
@@ -154,6 +135,20 @@ public class LevelSetup : MonoBehaviour
             var tmp = list[i];
             list[i] = list[r];
             list[r] = tmp;
+        }
+    }
+
+    private static void CreatePrefabs(int numberToCreate, Queue<Rect> grid, Vector2 jitter, MonoBehaviour prefab, Vector2 prefabSize)
+    {
+        var safeNumberToCreate = Mathf.Min(numberToCreate, grid.Count);
+        for (var i = 0; i < safeNumberToCreate; i++)
+        {
+            var cellCoordinates = grid.Dequeue();
+            var xJitter = Random.Range(-jitter.x, jitter.x);
+            var yJitter = Random.Range(-jitter.y, jitter.y);
+            var x = cellCoordinates.x + xJitter + prefabSize.x / 2;
+            var y = cellCoordinates.y + yJitter + prefabSize.y / 2;
+            Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
         }
     }
 }
